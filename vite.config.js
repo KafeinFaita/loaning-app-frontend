05eb68833bae597c97f123688d5e2db85881263c
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-16 11:21:15 +0800

    trying to fix proxy

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 import { config } from 'dotenv';
 
 // Call the `config` method if we're in a development environment
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   config();
+  console.log(process.env.NODE_ENV)
 }
-
-console.log(process.env.NODE_ENV)
 
 // https://vitejs.dev/config/
 export default defineConfig({
