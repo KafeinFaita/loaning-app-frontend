1623e74ee814b254832a63a549bcd611191c8d19
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-18 22:06:02 +0800

    added delete function on roles page

--- src/pages/Roles/RoleCreate.jsx
+++ src/pages/Roles/RoleCreate.jsx
@@ -13,21 +13,25 @@ const RoleCreate = () => {
         { name: "payments_allow_add", description: "Allow users with this role to add a new payment in the database", group: "payments" },
         { name: "payments_allow_edit", description: "Allow users with this role to edit a payment", group: "payments" },
         { name: "payments_allow_delete", description: "Allow users with this role to delete payments from the database", group: "payments" },
+
         // loan plans group
         { name: "loanplans_allow_view", description: "Allow users with this role to view the Loan Plans page", groupHead: true, group: "loanplans" },
         { name: "loanplans_allow_add", description: "Allow users with this role to add a new loan plan in the database", group: "loanplans" },
         { name: "loanplans_allow_edit", description: "Allow users with this role to edit a loan plan", group: "loanplans" },
         { name: "loanplans_allow_delete", description: "Allow users with this role to delete loan plans from the database", group: "loanplans" },
+
         // loan types group
         { name: "loantypes_allow_view", description: "Allow users with this role to view the Loan Types page", groupHead: true, group: "loantypes" },
         { name: "loantypes_allow_add", description: "Allow users with this role to add a new loan type in the database", group: "loantypes" },
         { name: "loantypes_allow_edit", description: "Allow users with this role to edit a loan type", group: "loantypes" },
         { name: "loantypes_allow_delete", description: "Allow users with this role to delete loan types from the database", group: "loantypes" },
+
         // user group
         { name: "users_allow_view", description: "Allow users with this role to view the Users page", groupHead: true, group: "user" },
         { name: "users_allow_add", description: "Allow users with this role to add a new user in the database", group: "user" },
         { name: "users_allow_edit", description: "Allow users with this role to edit a user's profile details", group: "user" },
         { name: "users_allow_delete", description: "Allow users with this role to delete user accounts from the database", group: "user" },
+
         //role group
         { name: "roles_allow_view", description: "Allow users with this role to view the Roles page", groupHead: true, group: "role" },
         { name: "roles_allow_add", description: "Allow users with this role to add a new roles in the database", group: "role" },
@@ -40,7 +44,7 @@ const RoleCreate = () => {
 
         try {
             const filteredPrivileges = checkboxRef.current.filter(box => box.checked).map(box => box.value);
-            const response = axios.post(`${import.meta.env.VITE_API_URL}/api/roles`, {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/roles`, {
                 title: titleRef.current,
                 privileges: filteredPrivileges
             })
--- src/pages/Roles/RoleIndex.jsx
+++ src/pages/Roles/RoleIndex.jsx
@@ -1,13 +1,41 @@
-import { useLoaderData } from "react-router-dom";
+import { useNavigate, useLoaderData, Link } from "react-router-dom";
+import axios from "axios";
 
 const Roles = () => {
     const roles = useLoaderData();
+    const navigate = useNavigate();
+
+    const handleDelete = async e => {
+        const roleId = e.target.getAttribute('role_id');
+
+        const response = await axios.delete(`${import.meta.env.VITE_API_URL}/api/roles/${roleId}`);
+        navigate('/dashboard/roles', { replace: true });
+    }
 
     return (
         <div>
-        {roles.data.map(role => {
-            return <h1>{role.title}</h1>
-        })}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Role Title</th>
+                        <th>Actions (NOT FUNCTIONAL YET)</th>
+                    </tr>
+                </thead>
+                <tbody>
+            {roles.data.map(role => {
+                return (
+                    <tr key={role.roleId} className="bg-gray-400"> 
+                        <td>{role.title}</td>
+                        <td>
+                            <Link to={role.roleId} className="text-white bg-gray-700 p-1 text-xs mx-2">Edit Role</Link>
+                            <button className="text-white bg-gray-700 p-1 text-xs" role_id={role.roleId} onClick={handleDelete}>Delete Role</button>
+                        </td>
+                    </tr>
+                )
+            })}     
+                </tbody>
+            </table>
+        
         </div>
     )
 } 
