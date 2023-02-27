9762df702ffa6f407f0ccc0128de22b52d1f5a0b
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-28 00:14:10 +0800

    completed loan crud

--- src/pages/Loans/LoanCreate.jsx
+++ src/pages/Loans/LoanCreate.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import LoadingScreen from '../../components/LoadingScreen';
+import FormSubmit from '../../components/FormSubmit';
 
 const LoanCreate = () => {
+    const navigate = useNavigate();
     const [grid, setGrid] = useState(null);
     const [loanTypes, setLoanTypes] = useState(null);
     const [selectedType, setSelectedType] = useState(null);
@@ -13,8 +16,6 @@ const LoanCreate = () => {
             try {
                 const grid = await axios.get(`${import.meta.env.VITE_API_URL}/api/loan-grid`);
                 const loanTypes = await axios.get(`${import.meta.env.VITE_API_URL}/api/loan-types`);
-
-                console.log(grid.data)
 
                 setGrid(grid.data);
                 setLoanTypes(loanTypes.data);
@@ -34,6 +35,22 @@ const LoanCreate = () => {
         setLoanData(grid.find(data => e.target.value <= data.maxLoan));
     }
 
+    const handleSubmit = async e => {
+        try {
+            e.preventDefault();
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/loans`, {
+                loanAmount: e.target.loanAmount.value,
+                loanType: e.target.loanType.value,
+                grid: loanData._id
+            }, { withCredentials: true })
+            console.log(response.data)
+            navigate('/dashboard/loans');
+        } catch (error) {
+            throw error;
+        }
+        
+    }
+
     if (!loanTypes) {
         return (
             <LoadingScreen />
@@ -41,24 +58,32 @@ const LoanCreate = () => {
     }
 
     return (
-        <form className='flex flex-col gap-4'>
-            <label htmlFor="loanType">
-                Loan to Apply 
-                <select name="loanType" id="loanType" onChange={handleTypeChange} className="block border-2">
-                    {loanTypes.map(type => <option value={type._id}>{type.name}</option>)}
-                </select>
-            </label>
+        <form onSubmit={handleSubmit}>
+            <div className='flex flex-col gap-7 mb-10'>
+                <label htmlFor="loanType">
+                    Loan to Apply 
+                    <select name="loanType" id="loanType" onChange={handleTypeChange} className="block border-2" required>
+                        <option selected disabled value=''>Select a loan type</option>
+                        {loanTypes.map(type => <option value={type._id}>{type.name}</option>)}
+                    </select>
+                </label>
 
-            <label htmlFor="loanAmount">
-                Loan Amount
-                <input type="number" name="loanAmount" id="loanAmount" onChange={handleLoanChange} className="block border-2" />
-            </label>
+                <label htmlFor="loanAmount">
+                    Loan Amount
+                    <input type="number" name="loanAmount" id="loanAmount" onChange={handleLoanChange} className="block border-2" required/>
+                </label>
 
-            <p>Maximum Loanable Amount: {selectedType ? selectedType.maxLoanAmount : null}</p>
-            <p>%Interest Rate Per Annum: {selectedType ? selectedType.interestRate : null}</p>
-            <p>Required Number of Co-Makers: {loanData ? loanData.coMakers : null}</p>
-            <p>Maximum Payment Terms: {loanData ? loanData.maxTerm : null} year/s</p>
-            <p>Processing Fee: Php {loanData ? loanData.processingFee : null}</p>
+                <p>Maximum Loanable Amount: {selectedType ? selectedType.maxLoanAmount : null}</p>
+                <p>%Interest Rate Per Annum: {selectedType ? selectedType.interestRate : null}</p>
+                <p>Required Number of Co-Makers: {loanData ? loanData.coMakers : null}</p>
+                <p>Maximum Payment Terms: {loanData ? loanData.maxTerm : 0} year/s</p>
+                <p>Processing Fee: Php {loanData ? loanData.processingFee : 0}</p>
+            </div>
+            
+
+            
+
+            <FormSubmit value="Submit Loan" />
         </form>
     )
 }
--- src/pages/Loans/LoanIndex.jsx
+++ src/pages/Loans/LoanIndex.jsx
@@ -1,6 +1,59 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import LoadingScreen from "../../components/LoadingScreen";
+
 const LoanIndex = () => {
+
+    const [loans, setLoans] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/loans`, { withCredentials: true });
+                console.log(response.data)
+                setLoans(response.data);
+            } catch (error) {
+                throw error
+            }   
+        }
+
+        fetchData();
+    }, [])
+
+    if (!loans) {
+        return <LoadingScreen />
+    }
+
     return (
-        <h1>LOANS</h1>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Member's Name</th>
+                        <th>Application Date</th>
+                        <th>Loan Applied</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {loans.map(loan => {
+                    return (
+                        <tr>
+                            <td>{loan.user.lastName}, {loan.user.firstName} {loan.user.middleName}</td>
+                            <td>{new Date(loan.createdAt).toLocaleString()}</td>
+                            <td>{loan.loanType.name}</td>
+                            <td>{loan.status}</td>
+                            <td>
+                                <Link to={loan.loanId} className="text-white bg-gray-700 p-1 text-xs mx-2">Details</Link>
+                            </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
