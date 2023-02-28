97b9924fc3956112ff143cd2cffcbc9afa2df097
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-28 22:25:20 +0800

    minor updates

--- src/pages/Roles/RoleEdit.jsx
+++ src/pages/Roles/RoleEdit.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
 const RoleEdit = () => {
+    const { id } = useParams();
+    const [role, setRole] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/roles/${id}`);
+                setRole(response.data);
+            } catch (error) {
+                
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
-        <h1>EDIT</h1>
+        <h1>{role ? role.title : id}</h1>
     )
 }
 
